65e37c63117c3f91f68daed8fbe6593c42fd6770
akcabra <andraz.klobucar@gmail.com>
2025-12-29 12:59:49 +0100

    HIKU-12: Added db migrations

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+CREATE SCHEMA IF NOT EXISTS scoreboards_challenges_service;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA scoreboards_challenges_service GRANT ALL PRIVILEGES ON TABLES TO hikuuser;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA scoreboards_challenges_service GRANT ALL PRIVILEGES ON SEQUENCES TO hikuuser;
+
