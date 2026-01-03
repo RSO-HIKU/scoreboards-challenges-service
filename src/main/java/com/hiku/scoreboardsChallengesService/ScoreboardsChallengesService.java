2b8cc12979ca67553157afe358c354964efc7a71
akcabra <andraz.klobucar@gmail.com>
2026-01-03 20:49:03 +0100

    HIKU-31: Urejeno za CICD in dodan Keycloak

--- src/main/java/com/hiku/scoreboardsChallengesService/ScoreboardsChallengesService.java
+++ src/main/java/com/hiku/scoreboardsChallengesService/ScoreboardsChallengesService.java
@@ -2,7 +2,11 @@ package com.hiku.scoreboardsChallengesService;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.eclipse.microprofile.auth.LoginConfig;
+import javax.annotation.security.DeclareRoles;
 
-@ApplicationPath("/scoreboards-challenges")
+@LoginConfig(authMethod = "MP-JWT")
+@DeclareRoles({"user", "admin"})
+@ApplicationPath("/api/scoreboards-challenges")
 public class ScoreboardsChallengesService extends Application {
 }
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,2 @@
 CREATE SCHEMA IF NOT EXISTS scoreboards_challenges_service;
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA scoreboards_challenges_service GRANT ALL PRIVILEGES ON TABLES TO hikuuser;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA scoreboards_challenges_service GRANT ALL PRIVILEGES ON SEQUENCES TO hikuuser;
-
